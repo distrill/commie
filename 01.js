44f4a5488e5630d0de31262d2bf26adbdb3f682a
Brent Hamilton <hamilton.bh9@gmail.com>
2021-12-01 13:14:25 -0800

    Update 01.js

--- 01.js
+++ 01.js
@@ -4,8 +4,6 @@ function go(textInput) {
         if (i < 3) return sum;
         const curr = e + input[i-1] + input[i-2];
         const prev = input[i-1] + input[i-2] + input[i-3];
-        console.log({curr, prev});
-        if (curr > prev) return sum+1;
-        return sum;
+        return curr > prev ? sum+1 : sum;
     }, 0);
 }
