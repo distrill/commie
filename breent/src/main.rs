0358cc098e882345d8fd850a36ed9e905c60db9c
Brent Hamilton <hamilton.bh9@gmail.com>
2021-12-02 18:57:02 -0800

    brent - problems dir, handle logging in parent

--- breent/src/main.rs
+++ breent/src/main.rs
@@ -1,17 +1,11 @@
 mod day;
-mod day_01;
-mod day_02;
+mod problems;
 
 use crate::day::Day;
-use crate::day_01::Day01;
-use crate::day_02::Day02;
+use crate::problems::day_01::Day01;
+use crate::problems::day_02::Day02;
 
 fn main() {
-    let day_01 = Day01::new();
-    day_01.run_01();
-    day_01.run_02();
-
-    let day_02 = Day02::new();
-    day_02.run_01();
-    day_02.run_02();
+    Day01::new().run();
+    Day02::new().run();
 }
