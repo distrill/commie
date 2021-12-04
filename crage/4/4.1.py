090b4a5526d3ec65648d7e7eb0fd8755976d1153
MilesWilde <craigmilde_@hotmail.com>
2021-12-04 16:52:10 -0500

    4.1

---
+++ crage/4/4.1.py
@@ -0,0 +1,94 @@
+from io import TextIOWrapper
+from math import sqrt
+
+class Node():
+    x: int
+    y: int
+    checked: bool
+    def __init__(self, x: int, y:int):
+        self.x = x
+        self.y = y
+        self.checked = False
+
+class Board():
+    nodes: dict[int, Node]
+    columnCounts: dict[int, int]
+    rowCounts: dict[int, int]
+    sideLength: int
+
+    def __init__(self, values: list[list[int]]):
+        self.columnCounts: dict[int, int] = {}
+        self.rowCounts: dict[int, int] = {}
+        self.nodes = self.initializeNodes(values)
+        self.sideLength = sqrt(len(self.nodes))
+
+    def initializeNodes(self, values: list[list[int]]):
+        nodes: dict[int, Node] = {}
+        for y in range(len(values)):
+            row = values[y]
+            for x in range(len(row)):
+                nodes[row[x]] = Node(x, y)
+        return nodes
+
+    def updateCounts(self, value: int):
+        if value in self.nodes:
+            node = self.nodes[value]
+            if not node.checked:
+                if node.x in self.columnCounts:
+                    self.columnCounts[node.x] += 1
+                else:
+                    self.columnCounts[node.x] = 1
+
+                if node.y in self.rowCounts:
+                    self.rowCounts[node.y] += 1
+                else:
+                    self.rowCounts[node.y] = 1
+                node.checked = True
+    
+    def checkIfBoardComplete(self, value: int):
+        if value in self.nodes:
+            node = self.nodes[value]
+            if self.columnCounts[node.x] == self.sideLength or self.rowCounts[node.y] == self.sideLength:
+                return True
+        return False
+
+    def sumBoardUnmarked(self):
+        sum = 0
+        for val, node in self.nodes.items():
+            if not node.checked:
+                sum += val
+        return sum
+
+def initializeBoards(file: TextIOWrapper):
+    twoBlankLineCount = 0
+    boards: list[Board] = []
+    currentBoardList: list[list[int]] = []
+    while twoBlankLineCount is not 2:
+        line = file.readline().rstrip()
+        if line == "":
+            twoBlankLineCount += 1
+            if len(currentBoardList) > 0:
+                boards.append(Board(currentBoardList))
+                currentBoardList = []
+        else:
+            twoBlankLineCount = 0
+            currentBoardLine = list(map(int, line.replace("  ", " ").strip().split(" ")))
+            currentBoardList.append(currentBoardLine)
+
+    return boards
+
+def playGame(filename: str):
+    with open(filename, 'r') as file:
+        bingoDrawsLine = file.readline().split(",")
+        bingoDraws = list(map(int, bingoDrawsLine))
+        boards: list[Board] = initializeBoards(file)
+        for bingoDraw in bingoDraws:
+            for board in boards:
+                board.updateCounts(bingoDraw)
+                if board.checkIfBoardComplete(bingoDraw):
+                    return board.sumBoardUnmarked() * bingoDraw
+
+print(playGame('./crage/4/input.txt'))
+    
+
+        
