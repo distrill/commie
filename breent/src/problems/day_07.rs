2678e525795cf7593a79321fc728743496592708
Brent Hamilton <hamilton.bh9@gmail.com>
2021-12-07 00:21:52 -0800

    day 7

---
+++ breent/src/problems/day_07.rs
@@ -0,0 +1,66 @@
+use std::collections::HashMap;
+
+use crate::day::Day;
+
+pub struct Day07 {}
+
+impl Day07 {
+    fn init_data(&self) -> Vec<i32> {
+        let data = &self.load_data();
+        data[0]
+            .split(",")
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect()
+    }
+
+    fn align_crabs<F>(&self, f: F) -> i32
+    where
+        F: Fn(i32, i32) -> i32,
+    {
+        let data = self.init_data();
+        // let data = self.init_data();
+        let mut counts = HashMap::new();
+
+        let mut max_sum = 0;
+        for i in 0..*data.iter().max().unwrap() {
+            if !counts.contains_key(&i) {
+                let mut sum = 0;
+                for j in data.iter() {
+                    sum += f(i, *j);
+                }
+                if sum < max_sum || max_sum == 0 {
+                    max_sum = sum;
+                }
+                counts.insert(i, sum);
+            }
+        }
+
+        max_sum
+    }
+}
+
+impl Day<i32> for Day07 {
+    fn new() -> Day07 {
+        Day07 {}
+    }
+
+    fn get_day_name(&self) -> String {
+        "07".to_string()
+    }
+
+    fn run_01(&self) -> i32 {
+        self.align_crabs(|x, y| (x - y).abs())
+    }
+
+    fn run_02(&self) -> i32 {
+        self.align_crabs(|x, y| {
+            let diff = (x - y).abs();
+            let mut sum = diff;
+            for i in 0..diff {
+                sum += i;
+            }
+            // println!("x: {}, y: {}, sum: {}", x, y, sum);
+            sum
+        })
+    }
+}
