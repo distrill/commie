17fd72ab4c8669b21cd22ac023841bc150aaf70b
Brent Hamilton <hamilton.bh9@gmail.com>
2021-12-08 00:16:52 -0800

    day 8

--- breent/src/problems/mod.rs
+++ breent/src/problems/mod.rs
@@ -5,6 +5,7 @@ mod day_04;
 mod day_05;
 mod day_06;
 mod day_07;
+mod day_08;
 
 pub use day_01::Day01;
 pub use day_02::Day02;
@@ -13,3 +14,4 @@ pub use day_04::Day04;
 pub use day_05::Day05;
 pub use day_06::Day06;
 pub use day_07::Day07;
+pub use day_08::Day08;
