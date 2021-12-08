e3c0c0b5d2522fe10a86f9f3c16b38e33cab81bb
Brent Hamilton <hamilton.bh9@gmail.com>
2021-12-08 00:18:27 -0800

    everythinginmain

--- breent/src/main.rs
+++ breent/src/main.rs
@@ -5,12 +5,12 @@ use crate::day::Day;
 
 fn main() {
     println!("!! AOC 2021 !!");
-    // problems::Day01::new().run();
-    // problems::Day02::new().run();
-    // problems::Day03::new().run();
-    // problems::Day04::new().run();
-    // problems::Day05::new().run();
-    // problems::Day06::new().run();
-    // problems::Day07::new().run();
+    problems::Day01::new().run();
+    problems::Day02::new().run();
+    problems::Day03::new().run();
+    problems::Day04::new().run();
+    problems::Day05::new().run();
+    problems::Day06::new().run();
+    problems::Day07::new().run();
     problems::Day08::new().run();
 }
